7ab0974adad223a1d0aa4a7e691b37b3050a3e30
Alessandro Sales <contato@alessandrosales.com.br>
2016-10-08 19:25:02 -0300

    Adicionei sass, coffee e configurei os arqivos de exemplo

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,38 @@
 var gulp = require('gulp');
 var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+var coffee = require('gulp-coffee');
 var connect = require('gulp-connect');
 
 var paths = {
-  pug: ['./src/pug/*.pug'],
-  pugDest: './www/',
-  pugWatch: ['./src/pug/**/*.pug'],
-  sass: ['./src/sass/**/*.sass'],
-  sassDest: './www/css/',
-  js: ['./src/js/**/*.js'],
-  jsDest: './www/js/'
+  pug: ['./src/pug/*.pug']
+  , pugDest: './www/'
+  , pugWatch: ['./src/pug/**/*.pug']
+  , sass: ['./src/sass/*.sass']
+  , sassWatch: ['./src/sass/**/*.sass']
+  , sassDest: './www/css/'
+  , coffee: ['./src/coffee/**/*.coffee']
+  , coffeeDest: './www/js/'
 }
 
 gulp.task('pug', function () {
   gulp.src(paths.pug)
       .pipe(pug())
       .pipe(gulp.dest(paths.pugDest))
+      .pipe(connect.reload());
+});
+
+gulp.task('sass', function (){
+  gulp.src(paths.sass)
+      .pipe(sass({outputStyle: 'compressed'}))
+      .pipe(gulp.dest(paths.sassDest))
+      .pipe(connect.reload());
+});
+
+gulp.task('coffee', function (){
+  gulp.src(paths.coffee)
+      .pipe(coffee({bare: true}))
+      .pipe(gulp.dest(paths.coffeeDest))
       .pipe(connect.reload());
 });
 
@@ -28,6 +45,8 @@ gulp.task('connect', function() {
 
 gulp.task('watch', function(){
   gulp.watch(paths.pugWatch, ['pug']);
+  gulp.watch(paths.sassWatch, ['sass']);
+  gulp.watch(paths.coffee, ['coffee']);
 });
 
-gulp.task('default', ['pug','connect','watch']);
+gulp.task('default', ['pug','sass','coffee','connect','watch']);
